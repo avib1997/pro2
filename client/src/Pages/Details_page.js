bc0756931da8dc280b334935eb6cdfabdd5a6725
Moti Brod <95278103+motibrod@users.noreply.github.com>
2025-03-18 16:36:17 -0400

    Update Details_page.js

--- client/src/Pages/Details_page.js
+++ client/src/Pages/Details_page.js
@@ -14,8 +14,8 @@ import { useLayoutEffect } from 'react'
 const Details = () => {
   const navigate = useNavigate()
   const [eventNum, setEventNum] = useState('')
-  const { eventName, setEventName, giftDetails, setGiftDetails, userId, event, eventId, setEvent, eventNumber, setEventId, setEventNumber } = useContext(Context)
-  const [amount, setAmount] = useState(0)
+  const [giftDetails, setGiftDetails] = useState({ name: '', phone: '', blessing: '', amount: '' });
+  const { eventName, setEventName, userId, event, eventId, setEvent, eventNumber, setEventId, setEventNumber } = useContext(Context)  const [amount, setAmount] = useState(0)
   const [errorMessage, setErrorMessage] = useState('')
   const [file, setFile] = useState(null)
 
